5813eaeb17a2a96fcb428b814b5666c0e71367a3
christopher <christopher.zelezny@mailbox.org>
2020-12-15 15:31:56 +0100

    implement exception and check boundaries

--- src/main/java/ManhattanDistance.java
+++ src/main/java/ManhattanDistance.java
@@ -1,8 +1,11 @@
 public class ManhattanDistance {
 
+    private final int MIN_COORDINATE_VALUE = Integer.MIN_VALUE/4;
+    private final int MAX_COORDINATE_VALUE = Integer.MAX_VALUE/4;
+
     public int of(Point start, Point end) {
 
-
+            validatePointsFrom(start,end);
             int xResult = Math.max(start.x, end.x) - Math.min(start.x, end.x);
             int yResult = Math.max(start.y, end.y) - Math.min(start.y, end.y);
             int result = Math.abs(xResult) + Math.abs(yResult);
@@ -11,13 +14,17 @@ public class ManhattanDistance {
 
         }
 
-        private void validatePoints(Point start, Point end){
-            if (start.x < 0 || start.y < 0 || end.x < 0 || end.y == 0){
+        private void validatePointsFrom(Point start, Point end){
+            if (start.x < MIN_COORDINATE_VALUE || start.x > MAX_COORDINATE_VALUE ||
+                    start.y < MIN_COORDINATE_VALUE || start.y > MAX_COORDINATE_VALUE ||
+                    end.x < MIN_COORDINATE_VALUE || end.x > MAX_COORDINATE_VALUE ||
+                    end.y < MIN_COORDINATE_VALUE || end.y > MAX_COORDINATE_VALUE
+            ){
                 throw new CorruptCoordinatesOfClassPointException("Corrupt coordinate values");
             }
         }
 
-    private class CorruptCoordinatesOfClassPointException extends RuntimeException {
+    public class CorruptCoordinatesOfClassPointException extends RuntimeException {
         public CorruptCoordinatesOfClassPointException(String message) {
             super(message);
         }
--- src/test/java/ManhattanDistanceTest.java
+++ src/test/java/ManhattanDistanceTest.java
@@ -16,65 +16,76 @@ class ManhattanDistanceTest {
 
     @Test
     void manhattanDistance_should_return_4_on_x_coordinate() {
-        int result = manhattanDistance.of(new Point(5,0), new Point(1, 0));
+        int result = manhattanDistance.of(new Point(5, 0), new Point(1, 0));
         assertEquals(4, result);
     }
 
     @Test
     void manhattanDistance_should_return_1_on_y_coordinate() {
-        int result = manhattanDistance.of(new Point(0,100) , new Point(0, 99));
+        int result = manhattanDistance.of(new Point(0, 100), new Point(0, 99));
         assertEquals(1, result);
     }
 
     @Test
-    @DisplayName("test negative values")
-    void testNegativeValues() {
-        int result = manhattanDistance.of(new Point(-100, -100), new Point(100,100));
-        assertEquals(400, result );
+    void manhattanDistance_should_return_400_on_negative_and_positive_points() {
+        int result = manhattanDistance.of(new Point(-100, -100), new Point(100, 100));
+        assertEquals(400, result);
     }
 
     @Test
-    @Disabled
-    @DisplayName("test negative maximum value of x coordinate from the start point")
-    void testNegativeMaximumValueOfXFromTheStartPoint() {
-        int result = manhattanDistance.of(new Point(Integer.MIN_VALUE, 0), new Point(0,0));
-        assertEquals(Math.abs(Integer.MIN_VALUE), result);
-    }
-    @Test
-    @Disabled
-    @DisplayName("test negative maximum value of x coordinate from the end point")
-    void testNegativeMaximumValuesOfXFromTheEndPoint() {
-        int result = manhattanDistance.of(new Point(0, 0), new Point(Integer.MIN_VALUE,0));
-        assertEquals(Math.abs(Integer.MIN_VALUE), result);
+    void manhattanDistance_should_validate_coordinate_points_from_start_point_on_x_coordinate_and_throw_exception() {
+        Exception exception = assertThrows(
+                ManhattanDistance.CorruptCoordinatesOfClassPointException.class,
+                () -> manhattanDistance.of(new Point(Integer.MIN_VALUE, 0), new Point(0, 0)));
+
+        assertTrue(exception.getMessage().contains("Corrupt coordinate values"));
     }
 
     @Test
-    @Disabled
-    @DisplayName("test negative maximum value of y coordinate from the end point")
-    void testMaximumValuesOfYFromTheStartPoint() {
-        int result = manhattanDistance.of(new Point(0, Integer.MIN_VALUE), new Point(0,0));
-        assertEquals(Math.abs(Integer.MIN_VALUE), result);
+    void manhattanDistance_should_validate_coordinate_points_from_start_point_on_y_coordinate_and_throw_exception() {
+        Exception exception = assertThrows(
+                ManhattanDistance.CorruptCoordinatesOfClassPointException.class,
+                () -> manhattanDistance.of(new Point( 0, Integer.MAX_VALUE + 1), new Point(0, 0)));
+
+        assertTrue(exception.getMessage().contains("Corrupt coordinate values"));
     }
 
     @Test
-    @Disabled
-    @DisplayName("test negative maximum value of y coordinate from the end point")
-    void testMaximumValuesOFXFromTheEndPoint() {
-        int result = manhattanDistance.of(new Point(0, 0), new Point(0,Integer.MIN_VALUE));
-        assertEquals(Math.abs(Integer.MIN_VALUE), result);
+    void manhattanDistance_should_validate_coordinate_points_from_end_point_on_x_coordinate_and_throw_exception() {
+        Exception exception = assertThrows(
+                ManhattanDistance.CorruptCoordinatesOfClassPointException.class,
+                () -> manhattanDistance.of(new Point( 0, 0), new Point(Integer.MIN_VALUE/4 - 1 , 0)));
+
+        assertTrue(exception.getMessage().contains("Corrupt coordinate values"));
     }
 
     @Test
-    @Disabled
-    @DisplayName("test maximum negative and maximum positive vale from X coordinate")
-    void testMaximumNegativeAndMaximumPositiveValeFromXCoordinate() {
-        int result = manhattanDistance.of(new Point(Integer.MIN_VALUE,0), new Point(Integer.MAX_VALUE,0));
-        assertEquals(1,result );
+    void manhattanDistance_should_validate_coordinate_points_from_end_point_on_y_coordinate_and_throw_exception() {
+        Exception exception = assertThrows(
+                ManhattanDistance.CorruptCoordinatesOfClassPointException.class,
+                () -> manhattanDistance.of(new Point( 0, 0), new Point(0, Integer.MIN_VALUE)));
+
+        assertTrue(exception.getMessage().contains("Corrupt coordinate values"));
     }
 
 
+    @Test
+    void manhattanDistance_should_return_complete_Value_minus_one_of_datatype_integer() {
+        int result = manhattanDistance.of(new Point(Integer.MIN_VALUE/4, Integer.MIN_VALUE/4 ), new Point(Integer.MAX_VALUE/4, Integer.MAX_VALUE/4));
+        assertEquals(Integer.MAX_VALUE -1, result);
+    }
 
+    @Test
+    void manhattanDistance_should_return_complete_Value_minus_one_of_datatype_integer_with_inverse_values() {
+        int result = manhattanDistance.of(new Point(Integer.MAX_VALUE/4, Integer.MAX_VALUE/4 ), new Point(Integer.MIN_VALUE/4, Integer.MIN_VALUE/4));
+        assertEquals(Integer.MAX_VALUE -1, result);
+    }
 
+    @Test
+    void manhattanDistance_should_return_Zero_with_maximum_positive_boundary() {
+        int result = manhattanDistance.of(new Point(Integer.MAX_VALUE/4, Integer.MAX_VALUE/4 ), new Point(Integer.MAX_VALUE/4, Integer.MAX_VALUE/4));
+        assertEquals(0, result);
+    }
 
 
 
